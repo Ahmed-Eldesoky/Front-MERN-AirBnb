994dd90acd8acf0641a7bb4c75eda85d951410d6
Ahmed Eldesoky <ahmedeldesoky026@gmail.com>
2021-02-22 22:51:04 +0200

    update\

--- frontend/src/components/search-result/search-result-item.js
+++ frontend/src/components/search-result/search-result-item.js
@@ -16,7 +16,7 @@ export default function SearchResultItem({ reservation }) {
         <div className="col-md-6">
           <img
             width="80%"
-            height="90%px"
+            height="256px"
             className='rounded'
             src={`http://localhost:4000/reservation/${_id}/image`}
             alt="apartment"
@@ -32,19 +32,6 @@ export default function SearchResultItem({ reservation }) {
               <p className="P_Price" style={{color:"#2F4F4F"}}><strong>{price}EGP </strong>/ night</p>
           </div>
       </div>
-        {/* <div className="col-md-6">
-          <div className="text-dark">
-            <p>{type}</p>
-            <h4 className="">{name}</h4>
-            <hr style={{width:"60px"}}/>
-            <p className="">{description}</p>
-            <p className="">
-              <div className="" style={{marginLeft:"180px"}}>
-                <strong>$ {price} EGP</strong>/Night
-              </div>
-            </p>
-          </div>
-        </div> */}
       </Link>
       <hr></hr>
     </div>
